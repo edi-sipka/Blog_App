4152dcfcfaa0659053b10e93e18e2163a2986e24
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-24 10:04:47 +0100

    Fix bugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,17 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+
+  after_save :update_post_counter
+
+  private 
   def update_post_counter
     author.increment!(:posts_counter)
   end
 
-  def recent_comments
-    Comment.order(created_at: :desc).includes(:post).limit(5)
-  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
 
   def recent_posts
-    Post.order(created_at: :desc).includes(:author).limit(3)
+    posts.order(created_at: :desc).limit(3)
   end
 end
