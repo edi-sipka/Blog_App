2bc1f69bed608625ed0f45e5d7f6a5b5123249f8
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-24 10:10:14 +0100

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,13 +7,11 @@ class Post < ApplicationRecord
     comments.order(created_at: :desc).limit(5)
   end
 
-
   after_save :update_post_counter
 
-  private 
+  private
+
   def update_post_counter
     author.increment!(:posts_counter)
   end
-
-  
 end
