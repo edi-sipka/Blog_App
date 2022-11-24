81ea2d3c212ef7df9dfb7ccc5da18a39428ac98e
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-24 12:42:00 +0100

    Add post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  validates :title, presence: true, length: {maximum: 250}
+  validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: {only_integer:true}
+  validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: {only_integer:true}
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
