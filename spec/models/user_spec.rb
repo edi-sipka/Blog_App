4c15b6589d96df9fd0c59f00aeabee4b44efc239
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-24 12:55:40 +0100

    Rspec user testing

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  subject {User.new(name: "Edi", photo: "https://avatars.githubusercontent.com/u/105216647?v=4", posts_counter: 10,  bio: "Full-stack developer")}
+
+  before { subject.save }
+
+  it "Name should be present" do
+    subject.name = nil 
+    expect(subject).to_not be_valid
+  end
+
+  it "Post Counter must be integer" do
+    subject.posts_counter = "z"
+    expect(subject).to_not be_valid
+  end
+
+  it "Post Counter must be 0 or greater" do
+    subject.posts_counter = -5
+    expect(subject).to_not be_valid
+  end
+end
