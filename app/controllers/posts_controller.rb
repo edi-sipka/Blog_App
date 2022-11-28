3b9a8470796852e88ea76aff4704cf0aad6ed9e3
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-28 15:06:16 +0100

    Add controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    params[:id]
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+    params[:user_id]
+  end
+end
