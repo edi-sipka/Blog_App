2a00a175bb1d6387807830b389f5368ff6592d06
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-28 15:12:24 +0100

    Fix linters error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   def index; end
 
-  def show
-  end
+  def show; end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def index; end
 
-  def show
-  end
+  def show; end
 end
