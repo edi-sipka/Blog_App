992be006fc6beb1943f2a2c8c60312d5df216a35
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-29 11:17:55 +0100

    Add request testing

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+Rspec.describe 'Posts', type: :request do
+  describe 'GET/index' do
+    it 'Returns http success message and view' do
+      get '/users/:user_id/posts'
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include('List of all posts by given user')
+    end
+  end
+
+  describe 'GET/show' do
+    it 'Returns http success message and view' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include('List of specific post of specific user')
+    end
+  end
+end
