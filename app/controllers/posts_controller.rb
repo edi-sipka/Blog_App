a07f9de2bc00c0722d1975717b44f616f67e2174
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-30 18:18:30 +0100

    Add posts/:id page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @pagy, @posts = pagy(@user.posts,  items: 2)
+    @pagy, @posts = pagy(@user.posts, items: 2)
   end
 
   def show
-  end 
+    @user = User.find(params[:user_id])
+    @posts = Post.find(params[:id])
+    @comments = @posts.comments
+  end
 end
