aaf4220cb7573f02428dd40f38fa0dc2d1399109
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-02 11:53:39 +0100

    Refactor author id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
     @comments.author_id = Current.user.id
 
     if @comments.save
-      redirect_to "/users/#{@current_post.author.id}/posts/#{@current_post.id}"
+      redirect_to "/users/#{@current_post.author_id}/posts/#{@current_post.id}"
     else
       render :new
     end
