5179228382268a8ce1172a6dcb89384e0a3199fe
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-14 13:51:09 +0100

    Bullet bug fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @pagy, @posts = pagy(@user.posts.includes(:comments), items: 2)
+    @pagy, @posts = pagy(@user.posts, items: 2)
   end
 
   def show
