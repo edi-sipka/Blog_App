b0e1c2c287119de32c54eaaaeb062eeae27cec0f
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-15 14:37:49 +0100

    Fix post.rb bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,7 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   after_save :update_post_counter
-  load_and_authorize_resource
-
+  
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
   validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
@@ -25,3 +24,5 @@ class Post < ApplicationRecord
     author.increment!(:posts_counter)
   end
 end
+
+
