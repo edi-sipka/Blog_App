05376d3da555466f0b5cd0f4cd49c847647019c3
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-15 14:34:25 +0100

    Rubocop fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
- 
 
   after_save :update_comments_counter
 
@@ -14,6 +13,4 @@ class Comment < ApplicationRecord
   def update_comments_counter
     post.increment!(:comments_counter)
   end
-end 
-
-
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -25,5 +25,3 @@ class Post < ApplicationRecord
     author.increment!(:posts_counter)
   end
 end
-
-
