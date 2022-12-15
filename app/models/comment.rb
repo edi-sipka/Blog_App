7addfa43124d9d9380a074d49842d370070aa844
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-15 13:47:09 +0100

    Add authorize resource to post and comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  load_and_authorize_resource
 
   after_save :update_comments_counter
 
